2bb8c61feada9fc40db2cf2073654002f83ec012
Gaboriaud <Gaboriaud@192.168.1.12>
2015-04-15 00:53:19 +0200

    * Fix Connection VM Prod

--- index.php
+++ index.php
@@ -16,16 +16,67 @@
                 <link href="css/skeleton.css" rel="stylesheet" type="text/css" media="all" />
 	</head>
 	<body>
-	<div class="container">
             <div class="row">
-                <div class="eight columns red_panel">
-                
-                </div> 
-                <div class="four columns">
-                Je suis passé par la
+                <div class="top-bar">
+                    <div class="u-pull-left">
+                        <h1>Steamental</h1>
+                    </div>   
+                    <div class="u-pull-right">
+                        Ressources
+                        <table>
+                            <tr>
+                                <td>Quartz</td>
+                                <td>Wood</td>
+                                <td>Food</td>
+                            </tr>
+                            <tr>
+                                <td>Gold</td>
+                                <td>Silk</td>
+                                <td>Pop</td>
+                            </tr>
+                        </table>
+                    </div> 
                 </div> 
             </div>  
 	
-	</div>
+               
+            <div class="row">
+                <div class="three columns dashboard_panel">
+                    <ul class="menu-vertical tabs">
+                        <li class="mv-item"><a href="#tab1">Palais</a></li>
+                        <li class="mv-item"><a href="#tab2">Caserne</a></li>
+                        <li class="mv-item"><a href="#tab3">Laboratoire</a></li>
+                        <li class="mv-item"><a href="#tab4">Forêt Enchantée</a></li>
+                    </ul>
+                       
+                </div> 
+                <div id="tab1" class="six columns main_panel">
+                    Main 1
+                </div> 
+                <div id="tab2" class="six columns main_panel">
+                    Main 2
+                </div> 
+                <div id="tab3" class="six columns main_panel">
+                    Main 3
+                </div> 
+                <div id="tab4" class="six columns main_panel">
+                    Main 4
+                </div> 
+                
+                <div class="three columns production_panel">
+                    <br>Productions en cours :
+                        <br>Mousquet
+                        <br>Forêt Enchantée niv 2
+                    <br><br>Recherches en cours :
+                        <br>Foreuses en crystal
+                        <br>Bipate à vapeur
+                    <br><br>Ressources produites
+                        <br>Minerai : +0.8/s
+                        <br>Bois : +3.6/s
+                        <br>Or : +0.08/s
+                        <br>Quartz : +1.1/s
+                </div> 
+            </div>
 	</body>
 </html>
+<script type="text/javascript" language="javascript" src="js/tabs.js"></script>
